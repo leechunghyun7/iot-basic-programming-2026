162bb4892eba32d2f63f68d07ab280474403f18a
karunamin <freedom12528@gmail.com>
2026-02-09 08:49:02 +0900

    Update main.c

--- Day04/Chap-12-solution/Chap-12-01-app/main.c
+++ Day04/Chap-12-solution/Chap-12-01-app/main.c
@@ -4,10 +4,14 @@
 
 int main(void) {
 	//문자열 출력
-	printf("%s\n", "apple");
+	printf("%u\n", "apple");
 	//문자열 주소 확인
 	printf("%u\n", "apple");
 	printf("두번째문자 주소%u\n", "apple +1");
+	printf("%u\n", "apple");
+	printf("%u\n", "apple");
 
+	fruit = "banan"; // 무낮 배열으 ㅣ값을 변경할 숭 ㅓㅄ음.
+	strcpy(fruit,)
 	return 0;
 }
