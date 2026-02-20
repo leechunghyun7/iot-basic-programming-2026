ea253123edb4f32040d7439b37a19d82c61329e2
karunamin <freedom12528@gmail.com>
2026-02-20 09:32:02 +0900

    d
    d

--- Day10/ToyProject01/Addressbook_step5/addressbook.h
+++ Day10/ToyProject01/Addressbook_step5/addressbook.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef ADDRESSBOOK_H
+#define ADDRESSBPPK_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CONTRACTS 100
+#define NAME_LEN 31
+#define PHONE_LEN 20
+#define EMAIL_LEN 65
+#define ADDR_LEN 129
+#define MEMO_LEN 129
+
+typedef struct _contact {
+    char name[NAME_LEN];
+    char phone[PHONE_LEN];
+    char address[ADDR_LEN];
+    char email[EMAIL_LEN];
+    char memo[MEMO_LEN];
+} Contact;//이후 Contact 이름으로 사용
+
+#endif
---
+++ Day10/ToyProject01/Addressbook_step5/소스.cpp
@@ -0,0 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "addressbook.h"
+
+#pragma region 변수 영역
